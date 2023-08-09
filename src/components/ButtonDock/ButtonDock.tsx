443907d3c611021dd1b9850554251e7bbbc1348f
AngadSandhu <angad.sandhu16@gmail.com>
2023-08-09 09:49:50 +0530

    Adding icons to button dock

--- src/components/ButtonDock/ButtonDock.tsx
+++ src/components/ButtonDock/ButtonDock.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import bdStyles from "./ButtonDock.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { faRightLong } from "@fortawesome/free-solid-svg-icons";
 
 const ButtonDock = () => {
   return (
     <React.Fragment>
       <div className={bdStyles.bdContainer}>
-        <button className={bdStyles.bdButton}> Left</button>
-        <button className={bdStyles.bdButton}>Right</button>
+        <button className={bdStyles.bdButton}>
+          {" "}
+          <FontAwesomeIcon icon={faLeftLong} />{" "}
+        </button>
+        <button className={bdStyles.bdButton}>
+          {" "}
+          <FontAwesomeIcon icon={faRightLong} />{" "}
+        </button>
       </div>
     </React.Fragment>
   );
